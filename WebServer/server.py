48e8ff704ca965f06846e2b249644d39513aa5d7
DavidTausend <davidtausend-911@hotmail.com>
2023-08-26 13:09:00 +0000

    adding user and post_id

--- WebServer/server.py
+++ WebServer/server.py
@@ -7,6 +7,10 @@ print(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route("/user/<username>/<int:post_id>")
+def user(username=None, post_id=None):
+    return render_template('index.html', name=username, post_id=post_id)
+
 @app.route("/blog")
 def blog():
     return "These are my thoughts on blogs"
