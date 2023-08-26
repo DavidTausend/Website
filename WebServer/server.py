3a01bc09171e698cd202de20c8a21c3b1fa218ce
DavidTausend <davidtausend-911@hotmail.com>
2023-08-26 18:26:20 +0000

    added web template

--- WebServer/server.py
+++ WebServer/server.py
@@ -4,16 +4,21 @@ app = Flask(__name__)
 print(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template('index.html')
+print(home)
 
-@app.route("/user/<username>/<int:post_id>")
-def user(username=None, post_id=None):
-    return render_template('index.html', name=username, post_id=post_id)
+# @app.route("/about.html")
+# def about():
+#     return render_template('about.html') 
 
-@app.route("/blog")
-def blog():
-    return "These are my thoughts on blogs"
+# @app.route("/user/<username>/<int:post_id>")
+# def user(username=None, post_id=None):
+#     return render_template('index.html', name=username, post_id=post_id)
+
+# @app.route("/blog")
+# def blog():
+#     return "These are my thoughts on blogs"
 
 # @app.route("/favicon.ico")
 # def blog():
