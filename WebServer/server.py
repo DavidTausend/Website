255112da193e3754102885d9d748877c8c554db6
DavidTausend <davidtausend-911@hotmail.com>
2023-08-27 14:56:10 +0000

    adding dynamic html

--- WebServer/server.py
+++ WebServer/server.py
@@ -6,20 +6,9 @@ print(__name__)
 @app.route("/")
 def home():
     return render_template('index.html')
-print(home)
 
-# @app.route("/about.html")
-# def about():
-#     return render_template('about.html') 
 
-# @app.route("/user/<username>/<int:post_id>")
-# def user(username=None, post_id=None):
-#     return render_template('index.html', name=username, post_id=post_id)
+@app.route("/<string:page_name>")
+def html_page(page_name):
+     return render_template('page_name') 
 
-# @app.route("/blog")
-# def blog():
-#     return "These are my thoughts on blogs"
-
-# @app.route("/favicon.ico")
-# def blog():
-#     return "These are my thoughts on blogs"
