dbc441ba5e3d0cf27ec4f8fba35eb10334071132
DavidTausend <davidtausend-911@hotmail.com>
2023-08-28 14:07:01 +0000

    adding database

--- WebServer/server.py
+++ WebServer/server.py
@@ -12,11 +12,18 @@ def home():
 def html_page(page_name):
      return render_template(page_name) 
 
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        name = data["name"]
+        email = data["email"]
+        message = data["message"]
+        file = database.write(f'\n{name},{email},{message}')
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
+        write_to_file(data)
         return redirect('/thankyou.html')
     else:
         return 'something went wrong.Try again'
