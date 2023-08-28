ca11d49e4e81aabac47dab9fba1d0f7da69de0e8
DavidTausend <davidtausend-911@hotmail.com>
2023-08-28 15:38:33 +0000

    adding database

--- WebServer/server.py
+++ WebServer/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
+import csv
 
 app = Flask(__name__)
 print(__name__)
@@ -18,6 +19,15 @@ def write_to_file(data):
         email = data["email"]
         message = data["message"]
         file = database.write(f'\n{name},{email},{message}')
+
+def writ_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2:
+        name = data["name"]
+        email = data["email"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(name,email,message)
+
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
